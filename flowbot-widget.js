6ce9e4596b9ac6ee7f04a9e5d586c22a78383af2
ola-9 <nepogodda@gmail.com>
2024-01-15 15:53:43 +0300

    update

---
+++ flowbot-widget.js
@@ -0,0 +1,50 @@
+const runFlowbot = (state) => {
+  const start = state.find((item) => {
+    return item.id === 'welcome';
+  });
+  
+  let currentState = start;
+  const messages = [start.message];
+  
+  const root = document.getElementById('root');
+  const messagesEl = document.querySelector('.messages');
+  const buttonsEl = document.querySelector('.buttons');
+  
+  const handleClick = (nextState) => {
+    const res = state.find((item) => item.id === nextState);
+    messages.push(res.message);
+    currentState = res;
+    console.log('click');
+    renderMessages(messages);
+    renderButtons(currentState);
+  }
+  
+  const renderMessages = (messages) => {
+    messages.forEach((item, index) => {
+      const p = document.createElement('p');
+      p.textContent = item;
+      messagesEl.appendChild(p);
+  
+      if (index === messages.length - 1) {
+        p.scrollIntoView();
+      }
+    });
+  }
+  
+  const renderButtons = (state) => {
+      buttonsEl.innerHTML = '';
+      state.buttons.forEach((btn) => {
+      const button = document.createElement('button');
+      button.textContent = btn.text;
+      button.type = 'button'; 
+      button.classList.add('btn', 'btn-primary'); 
+      button.addEventListener('click', () => handleClick(btn.next_state_id));
+      buttonsEl.appendChild(button);
+    })
+  }
+  
+  
+  renderMessages(messages);
+  
+  renderButtons(currentState);
+}
